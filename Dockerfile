77bd440d1623380dc720efd72beb162353090ae4
Dmitry Lavrov <lavrov.webdev@gmail.com>
2024-01-18 09:58:31 +0300

    replace cors url, add PORT env

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,13 @@ RUN yarn
 
 COPY . .
 
+RUN apt-get update && \
+    apt-get install wget postgresql-client --yes && \
+    mkdir --parents ~/.postgresql && \
+    wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" \
+         --output-document ./prisma/root.crt && \
+    chmod 0600 ./prisma/root.crt
+
 RUN yarn prisma migrate deploy && yarn prisma generate && yarn build
 
 CMD [ "node", "dist/main.js" ]
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Finances app')
-    .setDescription('App for managing your personal finances')
-    .setVersion('1.0')
-    .addCookieAuth('access_token')
+    .setTitle("Finances app")
+    .setDescription("App for managing your personal finances")
+    .setVersion("1.0")
+    .addCookieAuth("access_token")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup("api/docs", app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://bbabrjced7868pqsc83u.containers.yandexcloud.net/",
+    ],
     credentials: true,
   });
-  await app.listen(process.env.SERVER_PORT);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
