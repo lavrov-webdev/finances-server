7f46afea2c59bb357b5107b4de911534be680468
Dmitry Lavrov <lavrov.webdev@gmail.com>
2024-06-23 14:50:22 +0300

    add filter on active category to envelopes getAllByDate

--- src/envelopes/envelopes.service.ts
+++ src/envelopes/envelopes.service.ts
@@ -45,6 +45,9 @@ export class EnvelopesService {
             gt: date,
           },
         },
+        category: {
+          isActive: true
+        }
       },
       include: {
         sprint: {
